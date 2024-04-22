210ed7e108e0169f7f9989adbeacc434ee971630
walker xian <walkerxian@gmail.com>
2024-04-22 10:05:43 +0800

    Added an example of adding a job
    1: Schedule() method submits a job to the job system for scheduling.
    2: The actual timing of job execution is determined by the job system's scheduler.
    3: Complete() method is used to wait for a specific job to finish execution. It does not
    trigger the execution of the job but waits for the already submitted job to complete.

--- Assets/Scripts/AddJob.cs
+++ Assets/Scripts/AddJob.cs
@@ -1,20 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
-public struct AddJob : IJob
+
+public class AddJobTest : MonoBehaviour
 {
-    public float a;
-    public float b;
-    
-    //blittable types :com.unity.collections package
-    public NativeArray<float> result;
 
-    public void Execute()
+    public bool longRunningJob;
+    private JobHandle handle;
+    private NativeArray<float> result;
+    
+    
+    public struct AddJob : IJob
     {
-        result[0] = a + b;
+        public float a;
+        public float b;
+    
+        //blittable types :com.unity.collections package
+        public NativeArray<float> result;
+
+        public void Execute()
+        {
+            result[0] = a + b;
+        }
+    }
+
+    private void Start()
+    {
+        //NativeContainer requires explicit memory management
+        result = new NativeArray<float>(1, Allocator.Persistent);
+        var job = new AddJob
+        {
+            a = 1,
+            b = 2,
+            result = result
+        };
+
+        handle = job.Schedule();
+
+        if (!longRunningJob)
+        {
+            handle.Complete();
+            Debug.Log($"result = {result[0]}");
+        }
+    }
+
+    private void Update()
+    {
+        //A Job itself is not constrained by the Update() and can run across frames
+        if (handle.IsCompleted)
+        {
+            handle.Complete();
+            Debug.Log($"result = {result[0]}");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (result.IsCreated)
+            result.Dispose();
     }
 }
 
